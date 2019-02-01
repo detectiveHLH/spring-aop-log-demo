1b2f4dce4ffa3703318560578a6a55d6ffa6f6c1
hulunhao <545759184@qq.com>
2019-02-01 15:07:23 +0800

    Prepare for the complex type

--- api/src/main/java/spring/aop/log/demo/api/controller/HelloController.java
+++ api/src/main/java/spring/aop/log/demo/api/controller/HelloController.java
@@ -1,10 +1,8 @@
 package spring.aop.log.demo.api.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import spring.aop.log.demo.api.util.Log;
+import spring.aop.log.demo.api.util.TestDTO;
 
 /**
  * HelloController
@@ -15,13 +13,14 @@ import spring.aop.log.demo.api.util.Log;
 @RestController
 public class HelloController {
     @Log(type = "WARNING")
-    @GetMapping("test/{id}")
+    @PostMapping("test/{id}")
     public String test(
             @PathVariable(name = "id") Integer id,
             @RequestParam(name = "workOrderNumber", required = false) String workOrderNumber,
             @RequestParam(name = "userId") String userId,
-            @RequestParam(name = "name") String name
+            @RequestBody TestDTO testDTO
     ) {
+        System.out.println(testDTO);
         return "Hello" + id;
     }
 }
---
+++ api/src/main/java/spring/aop/log/demo/api/util/TestDTO.java
@@ -0,0 +1,16 @@
+package spring.aop.log.demo.api.util;
+
+import lombok.Data;
+
+/**
+ * TestDTO
+ *
+ * @author Lunhao Hu
+ * @date 2019-02-01 15:02
+ **/
+@Data
+public class TestDTO {
+    private String name;
+
+    private String email;
+}
